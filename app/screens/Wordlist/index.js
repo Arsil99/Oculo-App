b904937a2692e01dfc9939bef77f3c71e3b6758f
hetalkumar <hetalkumar.patel@groovyweb.co>
2023-07-12 16:57:58 +0530

    minor changes

--- app/screens/Wordlist/index.js
+++ app/screens/Wordlist/index.js
@@ -11,12 +11,16 @@ import {
   TouchableOpacity,
   Image,
   TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
 } from 'react-native';
 import styles from './styles';
 
 export default function Wordlist({ navigation }) {
   const [showImage, setShowImage] = useState(false);
   const [textInputValue, setTextInputValue] = useState('');
+  const IOS = Platform.OS === 'ios';
 
   const onNextButtonPress = () => {
     setShowImage(true);
@@ -25,69 +29,84 @@ export default function Wordlist({ navigation }) {
     setTextInputValue(text);
   };
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor={BaseColors.white} />
+    <KeyboardAvoidingView
+      behavior={IOS ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+      >
+        <StatusBar barStyle="dark-content" backgroundColor={BaseColors.white} />
 
-      <HeaderBar
-        HeaderText={'Word List'}
-        HeaderCenter
-        leftText={'Cancel'}
-        leftBtnPress={() => {
-          navigation.goBack();
-        }}
-      />
+        <HeaderBar
+          HeaderText={'Word List'}
+          HeaderCenter
+          leftText={'Cancel'}
+          leftBtnPress={() => {
+            navigation.goBack();
+          }}
+        />
 
-      <View style={styles.mainDiv}>
-        <View style={{ flex: showImage ? 0.1 : 0.2 }}>
+        <View style={styles.mainDiv}>
+          <View style={{ flex: showImage ? 0.1 : 0.2 }}>
+            {showImage ? (
+              <Text style={styles.subtitleText}>
+                Enter words list from previous screen
+              </Text>
+            ) : (
+              <Text style={styles.subtitleText}>
+                Please memorize the word list and enter it on the next screen
+              </Text>
+            )}
+          </View>
           {showImage ? (
-            <Text style={styles.subtitleText}>
-              Enter words list from previous screen
-            </Text>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                multiline
+                placeholder="Type or speak here ..."
+                value={textInputValue}
+                onChangeText={onTextInputChange}
+                style={styles.textInput}
+                textAlignVertical="top"
+                underlineColorAndroid="transparent"
+              />
+            </View>
+          ) : null}
+          {showImage ? (
+            <TouchableOpacity
+              style={styles.wordcontainer}
+              activeOpacity={BaseSetting.buttonOpacity}
+            >
+              <Image
+                source={Images.speechtotext}
+                resizeMode="contain"
+                style={styles.img}
+              />
+            </TouchableOpacity>
           ) : (
-            <Text style={styles.subtitleText}>
-              Please memorize the word list and enter it on the next screen
-            </Text>
+            <View style={styles.wordcontainer}>
+              <Text style={styles.titleText}>
+                Jacket Arrow Pepper Cotton Movie
+              </Text>
+            </View>
           )}
-        </View>
-        {showImage ? (
-          <View style={styles.textInputContainer}>
-            <TextInput
-              multiline
-              placeholder="Type or speak here ..."
-              value={textInputValue}
-              onChangeText={onTextInputChange}
-              style={styles.textInput}
-              textAlignVertical="top"
-              underlineColorAndroid="transparent"
-            />
-          </View>
-        ) : null}
-        {showImage ? (
-          <TouchableOpacity
-            style={styles.wordcontainer}
-            activeOpacity={BaseSetting.buttonOpacity}
+          <View
+            style={[styles.buttoncontainer, { flex: showImage ? 0.5 : 0.3 }]}
           >
-            <Image
-              source={Images.speechtotext}
-              resizeMode="contain"
-              style={styles.img}
-            />
-          </TouchableOpacity>
-        ) : (
-          <View style={styles.wordcontainer}>
-            <Text style={styles.titleText}>
-              Jacket Arrow Pepper Cotton Movie
-            </Text>
+            {!showImage ? (
+              <Button
+                shape="round"
+                title={'Next'}
+                onPress={onNextButtonPress}
+              />
+            ) : (
+              <Button shape="round" title={'Next'} />
+            )}
           </View>
-        )}
-        <View style={[styles.buttoncontainer, { flex: showImage ? 0.5 : 0.3 }]}>
-          {!showImage ? (
-            <Button shape="round" title={'Next'} onPress={onNextButtonPress} />
-          ) : (
-            <Button shape="round" title={'Next'} />
-          )}
         </View>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
