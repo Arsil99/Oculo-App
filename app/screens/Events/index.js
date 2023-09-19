be80f20917c2574a01cdc63d709b80a5f0ac5e27
Muhammad Arsil <arshil@groovyweb.co>
2023-09-19 15:30:40 +0530

    bug fixed

--- app/screens/Events/index.js
+++ app/screens/Events/index.js
@@ -7,7 +7,7 @@ import HeaderBar from '@components/HeaderBar';
 import { useSelector } from 'react-redux';
 import { BaseColors } from '@config/theme';
 import BaseSetting from '@config/setting';
-import { isEmpty, isArray } from 'lodash';
+import { isEmpty, isArray, isNumber, isNull } from 'lodash';
 import { getApiData } from '@utils/apiHelper';
 import { ScrollView } from 'react-native-gesture-handler';
 import NoData from '@components/NoData';
@@ -89,6 +89,41 @@ export default function Events({ navigation }) {
             <ActivityIndicator size={'large'} />
           ) : !isEmpty(eventDetails) && isArray(eventDetails) ? (
             eventDetails?.map((item, index) => {
+              let filled_count =
+                Number(
+                  item.symptom_info &&
+                    !isNull(item.symptom_info) &&
+                    item.symptom_info == 1
+                    ? 1
+                    : 0,
+                ) +
+                Number(
+                  item.immediate_recall &&
+                    !isNull(item.immediate_recall) &&
+                    item.immediate_recall == 1
+                    ? 1
+                    : 0,
+                ) +
+                Number(
+                  item.digit_recall &&
+                    !isNull(item.digit_recall) &&
+                    item.digit_recall == 1
+                    ? 1
+                    : 0,
+                ) +
+                Number(
+                  item.treatment_info &&
+                    !isNull(item.treatment_info) &&
+                    item.digit_recall == 1
+                    ? 1
+                    : 0,
+                );
+
+              let total_count =
+                Number(isNumber(item.symptom_info) ?? 1) +
+                Number(isNumber(item.immediate_recall) ?? 1) +
+                Number(isNumber(item.digit_recall) ?? 1) +
+                Number(isNumber(item.treatment_info) ?? 1);
               return (
                 <CardList
                   key={index}
@@ -115,22 +150,11 @@ export default function Events({ navigation }) {
                         )
                   }
                   image={Images.emoji1}
-                  data={item?.createdAt}
+                  data={item?.title}
                   status={`${
-                    (item.symptom_info ?? 1) +
-                      (item.immediate_recall ?? 1) +
-                      (item.digit_recall ?? 1) +
-                      (item.treatment_info ?? 1) ===
-                    4
-                      ? 'Completed'
-                      : 'Pending'
+                    filled_count === total_count ? 'Completed' : 'Pending'
                   }`}
-                  assessment={`Assessment ${
-                    Number(item.symptom_info ?? 1) +
-                    Number(item.immediate_recall ?? 1) +
-                    Number(item.digit_recall ?? 1) +
-                    Number(item.treatment_info ?? 1)
-                  }/4`}
+                  assessment={`Assessment ${filled_count} / ${total_count}`}
                 />
               );
             })
